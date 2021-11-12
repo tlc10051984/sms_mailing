8fe24050024493835e5cea9e2ab8cd62c30fed30
tlc10051984 <71798900+tlc10051984@users.noreply.github.com>
2021-11-12 14:04:58 +0300

    Delete form_client.php

--- form_client.php
+++
@@ -1,6 +0,0 @@
-<form action="<?php $_SERVER['REQUEST_URI']?>" method="POST">    
-   
-   <input type='text' value='REX' size="12" name='sms_name'></br></br>
-   <input type='submit' value='Заказать' class='button1' name='order'>
-
-</form>
