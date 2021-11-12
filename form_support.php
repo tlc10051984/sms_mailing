0457a5ff3fc14ff21af42765669026a77e24e94a
tlc10051984 <71798900+tlc10051984@users.noreply.github.com>
2021-11-12 14:05:07 +0300

    Delete form_support.php

--- form_support.php
+++
@@ -1,7 +0,0 @@
-<form action="<?php $_SERVER['REQUEST_URI']?>" method="POST"> 
-   <b>Дополнительный комментарий оператора</b><br/>
-   <textarea name="comment" rows="5" cols="45"></textarea><br/><br/>
-   <input type='submit' value='Подтвердить' class='button1' name='yes'>
-   <input type='submit' value='Отказать' class='button1' name='no'>
-
-</form>
